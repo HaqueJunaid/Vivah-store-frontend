02dd8f71b9643942f871b873c3bdd97e0dd74165
Junaid Haque <junaid132boss@gmail.com>
2026-07-03 02:45:46 -0700

    vite config env

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { env } from 'process'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       },
